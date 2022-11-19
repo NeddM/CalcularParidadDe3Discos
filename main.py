d53738b3ed1df7f23c022345f47f7993480098be
Nedd Chairi Muñoz && i=0 <91211902+NeddM@users.noreply.github.com>
2022-11-19 11:16:11 +0100

    agregada función para añadir los discos deseados

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
 def main():
 
+    print("""
+   _____      _            _                          _     _           _ 
+  / ____|    | |          | |                        (_)   | |         | |
+ | |     __ _| | ___ _   _| | ___    _ __   __ _ _ __ _  __| | __ _  __| |
+ | |    / _` | |/ __| | | | |/ _ \\  | '_ \\ / _` | '__| |/ _` |/ _` |/ _` |
+ | |___| (_| | | (__| |_| | | (_) | | |_) | (_| | |  | | (_| | (_| | (_| |
+  \\_____\\__,_|_|\\___|\\__,_|_|\\___/  | .__/ \\__,_|_|  |_|\\__,_|\\__,_|\\__,_|
+                                    | |                                   
+                                    |_|                                   
+               _____           _____ _____    _____                       
+              |  __ \\    /\\   |_   _|  __ \\  | ____|                      
+              | |__) |  /  \\    | | | |  | | | |__                        
+              |  _  /  / /\\ \\   | | | |  | | |___ \\                       
+              | | \\ \\ / ____ \\ _| |_| |__| |  ___) |                      
+              |_|  \\_/_/    \\_|_____|_____/  |____/                       
+                                                                          
+                https://www.github.com/NeddM                                                          
+    """)
+
     numeroDiscos = int(input("¿Cuantos díscos tiene tu RAID5?: "))
 
     if numeroDiscos < 4:
@@ -15,15 +34,16 @@ def main():
 
         for cifra in range(len(disco[1])):
             comparacion = 0
-            for i in range(numeroDiscos):
+            for discos in range(numeroDiscos):
 
-                comparacion += int(disco[i][cifra])
+                comparacion += int(disco[discos][cifra])
 
             if comparacion % 2 == 0:
                 paridad.append("0")
             elif comparacion % 2 == 1:
                 paridad.append("1")
 
+        print("El bloque de paridad es: ", end="")
         for cifra in paridad:
             print(cifra, end="")
 
